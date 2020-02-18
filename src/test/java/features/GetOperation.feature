db2c15ce7d3528288bf715440d5f0de465ccc629
vDrobko <Volodymyr.Drobko@west.com>
2020-02-18 15:23:48 +0200

    Implement JSON schema validator.

--- src/test/java/features/GetOperation.feature
+++ src/test/java/features/GetOperation.feature
@@ -1,10 +1,16 @@
 Feature:
-    Verify different GET operations using REST-assured
+  Verify different GET operations using REST-assured
 
-    Scenario: Verify one title of the post
-        Given I perform GET operation for "/posts"
-        Then I should see the title as "Appium"
+  Scenario: Verify one title of the post
+    Given I perform GET operation for "/posts"
+    Then I should see the title as "Appium"
 
-    Scenario: Verify all titles of the post
-        Given I perform GET operation for "/posts/"
-        Then I should see all posts titles
+  Scenario: Verify all titles of the post
+    Given I perform GET operation for "/posts/"
+    Then I should see all posts titles
+
+  Scenario: Verify author of the first post using JSON validation
+    Given I perform GET operation for "/posts/" with query parameter
+      | id |
+      | 1  |
+    Then I should see author name as "Taras" with JSON validation
--- src/test/java/steps/GetOperations.java
+++ src/test/java/steps/GetOperations.java
@@ -7,6 +7,10 @@ import io.restassured.response.Response;
 import io.restassured.response.ResponseOptions;
 import utils.RestAssuredExtension;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsIterableContaining.hasItem;
 
@@ -26,5 +30,23 @@ public class GetOperations {
     @Then("^I should see all posts titles$")
     public void iShouldSeeTheTitleAs() {
         BDDStyleClass.verifyGetAllPostsTitles();
+    }
+
+    @Given("^I perform GET operation for \"([^\"]*)\" with query parameter$")
+    public void iPerformGETOperationForWithQueryParameter(String url, DataTable dataTable) {
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put(dataTable.raw().get(0).get(0), dataTable.raw().get(1).get(0));
+
+        response = RestAssuredExtension.getResourceWithQueryParameter(url, queryParams);
+}
+
+//JSON SCHEMA VALIDATOR
+    @Then("^I should see author name as \"([^\"]*)\" with JSON validation$")
+    public void iShouldSeeAuthorNameAsWithJSONValidation(String authorName) {
+        String responseBody = response.getBody().asString();
+
+        assertThat(responseBody, matchesJsonSchemaInClasspath("db.json"));
+        System.out.println("Response body: \n" + responseBody);
+        assertThat("Verify that response contain author name " + authorName, responseBody.contains(authorName));
     }
 }
